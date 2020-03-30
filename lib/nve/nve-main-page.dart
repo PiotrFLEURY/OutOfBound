9354cd6a134bc7692595c3614b72d76ee6499706
Vande Perre Nathan <nathan.vande-perre@epitech.eu>
2020-03-30 16:03:11 +0200

    feat Hello World

--- lib/nve/nve-main-page.dart
+++ lib/nve/nve-main-page.dart
@@ -3,7 +3,19 @@ import 'package:flutter/material.dart';
 class NveMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return new MaterialApp(
+      home: new FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      body: Center(child: new Text(
+          "Hello World"
+      )
+    ));
   }
 }
