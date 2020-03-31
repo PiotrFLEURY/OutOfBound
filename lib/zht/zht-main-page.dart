ef24597d03393cf571f46e79f063679c514f6e71
zohra051 <senouci12@hotmail.fr>
2020-03-31 11:48:39 +0200

    MAJ

--- lib/zht/zht-main-page.dart
+++ lib/zht/zht-main-page.dart
@@ -4,9 +4,17 @@ class ZhtMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Material(
-      color : Colors.green,
-    );
+     return new MaterialApp(
+      title: 'OutOfBound Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('OutOfBound Application'),
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
+      ),
+);
     throw UnimplementedError();
   }
 }
