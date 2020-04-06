cb5b89f41d7fbeef7664860e5a2bedda61dda17a
Vande Perre Nathan <nathan.vande-perre@epitech.eu>
2020-04-06 16:03:37 +0200

    feat Boundary setting

--- lib/nve/nve-main-page.dart
+++ lib/nve/nve-main-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:location/location.dart';
 import 'package:provider/provider.dart';
 import 'package:geolocator/geolocator.dart';
@@ -12,21 +13,23 @@ LocationData _locationData;
 class NveMainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => Position()),
-    ],
-    child: Consumer<Position>(
-      builder: (context, position, _) {
-        return new MaterialApp(
-          home: MyStatefulWidget(),
-        );
-      },
-    ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => Position()),
+      ],
+      child: Consumer<Position>(
+        builder: (context, position, _) {
+          return new MaterialApp(
+            home: MyStatefulWidget(),
+          );
+        },
+      ),
     );
   }
 }
 
 class Position with ChangeNotifier {
+  int _boundary;
   double _startingLongitude;
   double _startingLatitude;
 
@@ -38,6 +41,10 @@ class Position with ChangeNotifier {
     _startingLatitude = latitude;
   }
 
+  set boundary(int boundary) {
+    _boundary = boundary;
+  }
+
   double get longitude {
     return _startingLongitude;
   }
@@ -45,185 +52,202 @@ class Position with ChangeNotifier {
   double get latitude {
     return _startingLatitude;
   }
-}
-  
-  class MyStatefulWidget extends StatefulWidget {
-    MyStatefulWidget({Key key}) : super(key: key);
-  
-    @override
-    FirstScreen createState() => FirstScreen();
+
+  int get boundary {
+    return _boundary;
   }
-  
-  class FirstScreen extends State<MyStatefulWidget> {
-    int _selectedIndex = 0;
-    final List<Widget> _children = [MyStatefulBluePage(), MyStatefulRedPage(), GreenPage()];
-  
-    void _onItemTapped(int index) {
-      setState(() {
-        _selectedIndex = index;
-      });
-    }
-  
-    @override
-    Widget build(BuildContext ctxt) {
-      return new Scaffold(
-        body: _children[_selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  FirstScreen createState() => FirstScreen();
+}
+
+class FirstScreen extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  final List<Widget> _children = [
+    MyStatefulBluePage(),
+    MyStatefulRedPage(),
+    MyStatefulGreenPage()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+      body: _children[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
           currentIndex: _selectedIndex,
           onTap: (index) => _onItemTapped(index),
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Start position")),
-            BottomNavigationBarItem(icon: Icon(Icons.arrow_back), title: Text("Actual position")),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("Settings")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text("Start position")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.arrow_back), title: Text("Actual position")),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), title: Text("Settings")),
           ]),
-      );
+    );
+  }
+}
+
+class MyStatefulBluePage extends StatefulWidget {
+  MyStatefulBluePage({Key key}) : super(key: key);
+
+  @override
+  BluePage createState() => BluePage();
+}
+
+class BluePage extends State<MyStatefulBluePage> {
+  double _distance = 0;
+
+  Future<void> getDistance() async {
+    _locationData = await location.getLocation();
+    if (Provider.of<Position>(context, listen: false).latitude != null &&
+        Provider.of<Position>(context, listen: false).longitude != null) {
+      _distance = await geo.distanceBetween(
+          _locationData.latitude,
+          _locationData.longitude,
+          Provider.of<Position>(context, listen: false).latitude,
+          Provider.of<Position>(context, listen: false).longitude);
     }
   }
 
-  class MyStatefulBluePage extends StatefulWidget {
-    MyStatefulBluePage({Key key}) : super(key: key);
-  
-    @override
-    BluePage createState() => BluePage();
+  @override
+  void initState() {
+    getDistance().then((result) {
+      setState(() {});
+    });
+    super.initState();
   }
 
-  class BluePage extends State<MyStatefulBluePage> {
-    double _distance = 0;
-
-    Future<void> getDistance() async {
-      _locationData = await location.getLocation();
-      if (Provider.of<Position>(context, listen: false).latitude != null && Provider.of<Position>(context, listen: false).longitude != null) {
-        _distance = await geo.distanceBetween(_locationData.latitude, _locationData.longitude, 
-                                              Provider.of<Position>(context, listen: false).latitude, 
-                                              Provider.of<Position>(context, listen: false).longitude);
-      }      
-    }
-  
-    @override
-    void initState() {
-      getDistance().then((result) {
-        setState(() {
-          
-        });
-      });
-      super.initState();
-    }
-
-    @override
-    Widget build(BuildContext ctxt) {
-      if (Provider.of<Position>(context, listen: false).latitude != null && Provider.of<Position>(context, listen: false).longitude != null) {
-        return new Scaffold(
+  @override
+  Widget build(BuildContext ctxt) {
+    if (Provider.of<Position>(context, listen: false).latitude != null &&
+        Provider.of<Position>(context, listen: false).longitude != null) {
+      return new Scaffold(
           backgroundColor: Colors.blue,
-          body: 
-            Column(
+          body: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 Center(
-                  child: 
-                    Text.rich(TextSpan(children: <TextSpan>[
-                      TextSpan(text: Provider.of<Position>(ctxt, listen: false).longitude.toString()),
-                      TextSpan(text: " / "),
-                      TextSpan(text: Provider.of<Position>(ctxt, listen: false).latitude.toString())
-                    ])),
-                  ),
+                  child: Text.rich(TextSpan(children: <TextSpan>[
+                    TextSpan(
+                        text: Provider.of<Position>(ctxt, listen: false)
+                            .longitude
+                            .toString()),
+                    TextSpan(text: " / "),
+                    TextSpan(
+                        text: Provider.of<Position>(ctxt, listen: false)
+                            .latitude
+                            .toString())
+                  ])),
+                ),
                 Center(
-                  child: 
-                    Text.rich(TextSpan(children: <TextSpan>[
-                      TextSpan(text: "actually at "),
-                      TextSpan(text: _distance.toInt().toString()),
-                      TextSpan(text: " meters from this point")
-                    ])),
-                  )
+                  child: Text.rich(TextSpan(children: <TextSpan>[
+                    TextSpan(text: "actually at "),
+                    TextSpan(text: _distance.toInt().toString()),
+                    TextSpan(text: " meters from this point")
+                  ])),
+                )
               ]));
     } else {
-        return new Scaffold(
-          backgroundColor: Colors.blue,
-        );
-      }
+      return new Scaffold(
+        backgroundColor: Colors.blue,
+      );
     }
   }
-  
-  class MyStatefulRedPage extends StatefulWidget {
-    MyStatefulRedPage({Key key}) : super(key: key);
-  
-    @override
-    RedPage createState() => RedPage();
-  }
-  
-  class RedPage extends State<MyStatefulRedPage> {
-    double _distance = 0;
+}
 
-    Future<void> askPermission() async {
-      _serviceEnabled = await location.serviceEnabled();
+class MyStatefulRedPage extends StatefulWidget {
+  MyStatefulRedPage({Key key}) : super(key: key);
+
+  @override
+  RedPage createState() => RedPage();
+}
+
+class RedPage extends State<MyStatefulRedPage> {
+  double _distance = 0;
+
+  Future<void> askPermission() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
       if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
-          return;
-        }
-      }
-  
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != PermissionStatus.granted) {
-          return;
-        }
-      }
-  
-      _locationData = await location.getLocation();
-      if (Provider.of<Position>(context, listen: false).latitude != null && Provider.of<Position>(context, listen: false).longitude != null) {
-        _distance = await geo.distanceBetween(_locationData.latitude, _locationData.longitude, 
-                                              Provider.of<Position>(context, listen: false).latitude, 
-                                              Provider.of<Position>(context, listen: false).longitude);
+        return;
       }
     }
-  
-    @override
-    void initState() {
-      askPermission().then((result) {
-        setState(() {
-          
-        });
-      });
-      super.initState();
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
     }
-  
-    @override
-    Widget build(BuildContext ctxt) {
-      if (_locationData != null) {
-        return new Scaffold(
+
+    _locationData = await location.getLocation();
+    if (Provider.of<Position>(context, listen: false).latitude != null &&
+        Provider.of<Position>(context, listen: false).longitude != null) {
+      _distance = await geo.distanceBetween(
+          _locationData.latitude,
+          _locationData.longitude,
+          Provider.of<Position>(context, listen: false).latitude,
+          Provider.of<Position>(context, listen: false).longitude);
+    }
+  }
+
+  @override
+  void initState() {
+    askPermission().then((result) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext ctxt) {
+    if (_locationData != null) {
+      return new Scaffold(
           backgroundColor: Colors.red,
-          body:
-          Column(
+          body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Center(
-                  child: Text.rich(TextSpan(children: <TextSpan>[
-                    TextSpan(text: _locationData.longitude.toString()),
-                    TextSpan(text: " / "),
-                    TextSpan(text: _locationData.latitude.toString())
-                  ])),
-                ),
-              Center(
-                child: RaisedButton(
-                  onPressed: () {
-                    Provider.of<Position>(context, listen: false).longitude = _locationData.longitude;
-                    Provider.of<Position>(context, listen: false).latitude = _locationData.latitude;
-                },
-                child: Text("Make as start"),
-                )
+                child: Text.rich(TextSpan(children: <TextSpan>[
+                  TextSpan(text: _locationData.longitude.toString()),
+                  TextSpan(text: " / "),
+                  TextSpan(text: _locationData.latitude.toString())
+                ])),
               ),
               Center(
-                  child: Text.rich(TextSpan(children: <TextSpan>[
-                    TextSpan(text: "actually at "),
-                    TextSpan(text: _distance.toInt().toString()),
-                    TextSpan(text: " meters from the starting point")
-                  ])),
-                ),
-        ],)
-      );
+                  child: RaisedButton(
+                onPressed: () {
+                  Provider.of<Position>(context, listen: false).longitude =
+                      _locationData.longitude;
+                  Provider.of<Position>(context, listen: false).latitude =
+                      _locationData.latitude;
+                },
+                child: Text("Make as start"),
+              )),
+              Center(
+                child: Text.rich(TextSpan(children: <TextSpan>[
+                  TextSpan(text: "actually at "),
+                  TextSpan(text: _distance.toInt().toString()),
+                  TextSpan(text: " meters from the starting point")
+                ])),
+              ),
+            ],
+          ));
     } else {
       return new Scaffold(
         backgroundColor: Colors.red,
@@ -232,14 +256,59 @@ class Position with ChangeNotifier {
   }
 }
 
-class GreenPage extends StatelessWidget {
+class MyStatefulGreenPage extends StatefulWidget {
+  MyStatefulGreenPage({Key key}) : super(key: key);
+
+  @override
+  GreenPage createState() => GreenPage();
+}
+
+class GreenPage extends State<MyStatefulGreenPage> {
+  TextEditingController _controller = new TextEditingController();
+  FocusNode _textNode = new FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _handleSubmitted(String finalinput) {
+    setState(() {
+      Provider.of<Position>(context, listen: false).boundary = int.parse(finalinput);
+      debugPrint(Provider.of<Position>(context, listen: false).boundary.toString());
+    });
+  }
+
+  _setBoundary() {
+    TextField _textField = new TextField(
+      controller: _controller,
+      onSubmitted: _handleSubmitted,
+      keyboardType: TextInputType.number,
+    );
+
+    FocusScope.of(context).requestFocus(_textNode);
+
+    return new RawKeyboardListener(
+        focusNode: _textNode,
+        child: _textField);
+  }
+
   @override
   Widget build(BuildContext ctxt) {
-    return new Scaffold(
-      backgroundColor: Colors.green,
-      body: Center(child: new Text(
-          "Green Page"
-      ))
-    );
+      return new Scaffold(
+          backgroundColor: Colors.green,
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: Text("Boundary :"),
+                ),
+                Center(
+                  child: _setBoundary(),
+                ),
+              ]
+          )
+      );
   }
 }
