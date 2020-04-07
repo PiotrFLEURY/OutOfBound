e2499b50d7b658649ca665f31baf7334b5cbb8b6
zohra051 <senouci12@hotmail.fr>
2020-04-07 20:27:39 +0200

    boundary in meters on setting sub-page

--- lib/zht/LocationProvider.dart
+++ lib/zht/LocationProvider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
 class LocationProvider with ChangeNotifier {
   LocationData _currentLocation;
   LocationData _startingPosition;
+  String  _value;
 
   /*Send MAJ*/
   set starting(LocationData value) {
@@ -16,7 +18,15 @@ class LocationProvider with ChangeNotifier {
     notifyListeners();
   }
 
+set meters(String value){
+  _value = value;
+  notifyListeners();
+}
+
   /*Save data*/
   get current => _currentLocation;
   get starting => _startingPosition;
+
+  get meters => _value;
+
 }
--- lib/zht/pages/SettingsPage.dart
+++ lib/zht/pages/SettingsPage.dart
@@ -1,13 +1,59 @@
+import 'package:OutOfBounds/zht/LocationProvider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-class SettingsPage extends StatelessWidget{
+class SettingsPage extends StatefulWidget{
+  
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+
+  TextEditingController value = new TextEditingController() ;
+
+
   @override
   Widget build(BuildContext context){
-    return new MaterialApp(
-         home: new Scaffold(
-        backgroundColor: Colors.green,
+
+
+  return Consumer<LocationProvider>(
+        builder: (context, _provider, _) {
+                      return Scaffold(
+               backgroundColor: Colors.green,
+             body: Center(
+               
+               child: Column( 
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             
+            children : [
+             new TextField(
+                decoration: InputDecoration(
+                    hintText: 'Boundary in meters'
+                  ),
+                    keyboardType: TextInputType.number,
+                    maxLength: 10,
+                    inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],           
+                    controller: value,
+                    onSubmitted: (String value){
+                      setState((){
+                      _provider.meters = value;
+                      print(_provider.meters);
+                    });               
+                    },
+                textInputAction: TextInputAction.done
+            ),
+             
+            
+                   
+            ],
+          ),
+        ),
         
-      ),
+      );
+        }
     
     );
   }
